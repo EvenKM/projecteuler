9a97db927574ad67ca57fb5398110d1c2a7ca73b
EvenKM <even.k.mjos@gmail.com>
2025-04-12 19:19:32 +0200

    First problem solved.

--- p1/main.c
+++ p1/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main() {
-	printf("Testing...\n");
+	int sum = 0;
+	for (int i = 1; i < 1000; i++) {
+		if (i % 3 == 0 || i % 5 == 0) {
+			sum += i;
+		}
+	}
+	printf("Sum of all the multiples of 3 or 5 below 1000: %d\n", sum);
 	return 0;
 }
